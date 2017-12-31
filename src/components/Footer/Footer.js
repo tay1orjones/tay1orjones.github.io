b44f6c36a9d153130e88d71b01b55758beeac172
Taylor Jones <taylor.jones826@gmail.com>
2017-12-31 01:30:26 -0600

    add site repo link to footer, closes #10

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,14 @@ import "./footer.css";
 
 export default () => (
   <footer className="footer">
-    <p>&copy; 2017 Taylor Jones</p>
+    <span className="footer-item">&copy; 2017 Taylor Jones</span>
+    <span className="footer-item">
+      built with <a href="https://www.gatsbyjs.org/">gatsbyjs.org</a>
+    </span>
+    <span className="footer-item">
+      <a href="https://github.com/tay1orjones/tay1orjones.github.io">
+        site source on github
+      </a>
+    </span>
   </footer>
 );
